918b0525e41e683df1362eab40ab8b7e761f9066
hadeelsalaq <hadeelsalaq123456>
2019-12-26 11:35:47 +0200

    the app adds and views notes and notebooks perfectly final

---
+++ app/src/main/java/com/mohanadalkrunz99/firebaseauth/IntroSliderActivity.java
@@ -0,0 +1,15 @@
+package com.mohanadalkrunz99.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroSliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+}
